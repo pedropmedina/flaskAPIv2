12883277616dc713b84cb37f18a51abd7c065a60
pedropmedina <pedrodelilia@gmail.com>
2019-03-10 18:00:30 -0400

    Add models/user.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,23 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 
 from .models import db
+
+bcrypt = Bcrypt()
 
 
 def create_app():
     # app instance
     app = Flask(__name__)
 
-    # db intialization
+    # db initialization
     db.init_app(app)
 
     # db migration
     Migrate(db, app)
+
+    # bcrypt initialization
+    bcrypt.init_app(app)
 
     return app
---
+++ src/models/user.py
@@ -0,0 +1,30 @@
+from . import db
+from .. import bcrypt
+
+
+class User(db.Model):
+    '''
+    User Model to create user table
+    '''
+
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(255), unique=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    hash_password = db.Column(db.String(100), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    @property
+    def password(self):
+        raise AttributeError('password is write only')
+
+    @password.setter
+    def password(self, password):
+        # hash password and return string as function returns <class 'bytes'>
+        self.hash_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.hash_password, password)
